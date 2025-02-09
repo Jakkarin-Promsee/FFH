aba6f1b6fe6f48aab03af15aa9a2b32291da9d12
Night <146769399+Jakkarin-Promsee@users.noreply.github.com>
2025-02-10 03:03:16 +0700

    Feature: Fire system

---
+++ Assets/Script/Fire.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour
+{
+    public GameObject waterEF;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Water")) // Check if the object is water
+        {
+            StartCoroutine(DestroyFire()); // Destroy the fire object
+        }
+    }
+
+    IEnumerator DestroyFire()
+    {
+        // Instantiate the water effect at the fire’s position
+        GameObject effect = Instantiate(waterEF, transform.position, Quaternion.identity);
+
+        Destroy(gameObject); // Destroy the fire object
+
+        yield return new WaitForSeconds(2f); // Wait 2 seconds before destroying effect
+
+        Destroy(effect); // Destroy the water effect
+    }
+}
