cde0574868cdb88ea5a2ad42ea1053e746a5503f
Night <146769399+Jakkarin-Promsee@users.noreply.github.com>
2025-02-10 11:31:47 +0700

    Fix: Starting Music Source Conflict

--- Assets/SlimUI/Modern Menu 1/Scripts/Audio/CheckMusicVolume.cs
+++ Assets/SlimUI/Modern Menu 1/Scripts/Audio/CheckMusicVolume.cs
@@ -1,14 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-namespace SlimUI.ModernMenu{
-	public class CheckMusicVolume : MonoBehaviour {
-		public void  Start (){
+namespace SlimUI.ModernMenu
+{
+	public class CheckMusicVolume : MonoBehaviour
+	{
+		public void Start()
+		{
+			if (!PlayerPrefs.HasKey("MusicVolume"))
+			{
+				PlayerPrefs.SetFloat("MusicVolume", 0.5f); // Default volume
+				PlayerPrefs.Save();
+			}
+
 			// remember volume level from last time
 			GetComponent<AudioSource>().volume = PlayerPrefs.GetFloat("MusicVolume");
 		}
 
-		public void UpdateVolume (){
+		public void UpdateVolume()
+		{
 			GetComponent<AudioSource>().volume = PlayerPrefs.GetFloat("MusicVolume");
 		}
 	}
